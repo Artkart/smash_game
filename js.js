74bb86580f22d582aec1781b36afa5b4fa51b428
Deividas Urbonavicius <Deividas.Urbonavicius@Fin-DeiUrb-MAC.lan>
2017-10-13 17:19:39 +0300

    smash game almost done

--- js.js
+++ js.js
@@ -2,18 +2,93 @@ var minion1 = document.getElementById('minion1');
 var minion2 = document.getElementById('minion2');
 var minion3 = document.getElementById('minion3');
 
+var result = 0;
+var level = 1;
+var i = 0;
+
 function startGame() {
-
-
   document.getElementById('startBlock').style.display = 'none';
   document.getElementById('gameHover').style.display = 'none';
+
+
+  document.getElementById('currentScore').style.display = 'block';
+  document.getElementById('currentScore').innerHTML = 0;
 
 init();
 }
 
 function init() {
-  for (i=0; i< 10; i++) {
-    minion1.style.height = 10 * i + '%';
+  level = 1;
+  result = 0;
+  growMinion()
+}
+
+
+function growMinion() {
+  setTimeout(function () {
+  if (minion1.offsetHeight  > 700){
+    // will be game over
+    gameOver();
+    return false;
   }
 
+    minion1.style.height = minion1.offsetHeight + 10 + 'px';
+    minion2.style.height = minion2.offsetHeight + 10 + 'px';
+    minion3.style.height = minion3.offsetHeight + 10 + 'px';
+    growMinion();
+  }, 400/level);
+}
+
+function smashMinion(goblin) {
+  document.getElementById(goblin).style.height = 100 + 'px';
+  result += 1;
+
+  if ( result % 10 === 0) {
+    levelUp(level);
+  }
+
+  /*if (result ==5){
+    level = 2;
+  } else if   (result == 10) {
+    level = 3;
+  } else if (result == 20) {
+    level = 4;
+  } else if (result == 30) {
+    level = 5;
+  } else if (result == 50) {
+    level = 6;
+  } */
+  document.getElementById('currentScore').innerHTML = result;
+}
+
+function levelUp() {
+  level++;
+  document.getElementById('levelUp').style.display = 'block';
+  document.getElementById('levelUp').innerHTML  = 'level up ' + level;;
+
+
+
+  setTimeout(function(){
+    document.getElementById('levelUp').style.display = 'none';
+
+  }, 300 );
+
+}
+
+function gameOver() {
+  minion1.style.height = minion2.style.height =  minion3.style.height =  '100px';
+
+  document.getElementById('gameHover').style.display = 'block';
+  document.getElementById('gameOverBlock').style.display = 'block';
+  document.getElementById('result').innerHTML = result;
+
+  result = 0;
+}
+
+function restartGame() {
+  document.getElementById('gameHover').style.display = 'none';
+  document.getElementById('gameOverBlock').style.display = 'none';
+  document.getElementById('currentScore').innerHTML = 0;
+
+  init();
 }
