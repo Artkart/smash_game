d6894a69ef73bc5b2e5559a63a3ff52b20e39b33
Deividas <u.deividas@hotmail.com>
2018-04-04 15:48:20 +0300

    style fixes

--- js.js
+++ js.js
@@ -1,7 +1,6 @@
 var minion1 = document.getElementById('minion1');
 var minion2 = document.getElementById('minion2');
 var minion3 = document.getElementById('minion3');
-
 var result = 0;
 var level = 1;
 var i = 0;
@@ -9,55 +8,36 @@ var i = 0;
 function startGame() {
   document.getElementById('startBlock').style.display = 'none';
   document.getElementById('gameHover').style.display = 'none';
-
-
   document.getElementById('currentScore').style.display = 'block';
   document.getElementById('currentScore').innerHTML = 0;
-
 init();
 }
-
 function init() {
   level = 1;
   result = 0;
   growMinion()
 }
-
-
 function growMinion() {
   setTimeout(function () {
-  if (minion1.offsetHeight  > 700){
+  if (minion1.offsetHeight  > 600 || minion2.offsetHeight  > 600 || minion3.offsetHeight  > 600)
+  {
     // will be game over
     gameOver();
     return false;
   }
-
-    minion1.style.height = minion1.offsetHeight + 10 + 'px';
-    minion2.style.height = minion2.offsetHeight + 10 + 'px';
-    minion3.style.height = minion3.offsetHeight + 10 + 'px';
+    minion1.style.height = minion1.offsetHeight + 1 + 'px';
+    minion2.style.height = minion2.offsetHeight + 1 + 'px';
+    minion3.style.height = minion3.offsetHeight + 1 + 'px';
     growMinion();
-  }, 400/level);
+  }, 50/level);
 }
 
-function smashMinion(goblin) {
-  document.getElementById(goblin).style.height = 100 + 'px';
+function smashMinion(currentMinion) {
+  document.getElementById(currentMinion).style.height = 100 + 'px';
   result += 1;
-
   if ( result % 10 === 0) {
-    levelUp(level);
+    levelUp();
   }
-
-  /*if (result ==5){
-    level = 2;
-  } else if   (result == 10) {
-    level = 3;
-  } else if (result == 20) {
-    level = 4;
-  } else if (result == 30) {
-    level = 5;
-  } else if (result == 50) {
-    level = 6;
-  } */
   document.getElementById('currentScore').innerHTML = result;
 }
 
@@ -65,23 +45,16 @@ function levelUp() {
   level++;
   document.getElementById('levelUp').style.display = 'block';
   document.getElementById('levelUp').innerHTML  = 'level up ' + level;;
-
-
-
   setTimeout(function(){
     document.getElementById('levelUp').style.display = 'none';
-
-  }, 300 );
-
+  }, 1000 );
 }
 
 function gameOver() {
-  minion1.style.height = minion2.style.height =  minion3.style.height =  '100px';
-
+  minion1.style.height = minion2.style.height =  minion3.style.height =  '130px';
   document.getElementById('gameHover').style.display = 'block';
   document.getElementById('gameOverBlock').style.display = 'block';
   document.getElementById('result').innerHTML = result;
-
   result = 0;
 }
 
